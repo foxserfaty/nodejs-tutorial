05e66ad09fedbf384fdc0363818c4b439d74a323
foxserfaty <huyban0000@gmail.com>
2024-07-08 17:18:58 +0700

    MVC refactoring.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ const express = require('express')
 const morgan = require('morgan')
 const sass = require('node-sass')
 const handlebars = require('express-handlebars')
+const exp = require('constants')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 //HTTP Logger 
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', handlebars.engine({
@@ -19,13 +27,9 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources\\views'))
 
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+// init route
+route(app)
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
